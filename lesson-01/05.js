770043da8619a494f1a6ea80147585099299049a
FrauFri <fr-fr-fr@mail.ru>
2023-12-12 20:42:02 +0200

    js2-2

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -1,10 +1,12 @@
 
 
-  let dayOfWeek = 'gjh';
-if(dayOfWeek === 'Sunday' && 'Suterday') {
-  console.log('weekend');
-} else if(dayOfWeek === 'Monday' && 'Thusday' && 'Friday') {console.log('day');
-}
-else {
-  console.log('no')
-}
+  let giveMeResult = (dayOfWeek) => {
+    if(dayOfWeek === 'Sunday' && 'Suterday') {
+      console.log('weekend');
+    } else if(dayOfWeek === 'Monday' && 'Thusday' && 'Friday') {console.log('day');
+    }
+    else {
+      console.log('no')
+    };
+  };
+  giveMeResult('Monday')
